58bec7d955eeb6ba6d9e9250499fc49901422750
Fabienne Kuntz <fabienne.kuntz04@gmail.com>
2025-12-17 11:12:47 +0100

    Replace animal data and move it to html file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,24 +7,44 @@ def load_data(file_path):
     return json.load(handle)
 
 
+def read_html_data():
+    with open("animals_template.html", "r",) as file:
+        html_template = file.read()
+        return html_template
+
+
 def filter_animals_data(animals_data):
     """Filters the animals_data only for specific given keys"""
+    output = ""
     for animal in animals_data:
         location_list = animal.get("locations")
         if "name" in animal:
-            print(f'Name: {animal["name"]}')
+            output += f'Name: {animal["name"]}\n'
         if "diet" in animal["characteristics"]:
-            print(f'Diet: {animal["characteristics"]["diet"]}')
+            output += f'Diet: {animal["characteristics"]["diet"]}\n'
         if location_list:
-            print(f'Location: {location_list[0]}')
+            output += f'Location: {location_list[0]}\n'
         if "type" in animal["characteristics"]:
-            print(f'Type: {animal["characteristics"]["type"]}')
-        print()
+            output += f'Type: {animal["characteristics"]["type"]}\n'
 
+    return output
+
+def replace_info(animals_data):
+    html_template = read_html_data()
+    new_html = html_template.replace("__REPLACE_ANIMALS_INFO__", filter_animals_data(animals_data))
+    return new_html
+
+def write_new_html_file(animals_data):
+    new_html = replace_info(animals_data)
+    with open("animals.html", "w") as file:
+        file.write(new_html)
 
 def main():
     animals_data = load_data('animals_data.json')
-    filter_animals_data(animals_data)
+    print(filter_animals_data(animals_data))
+    print(replace_info(animals_data))
+    write_new_html_file(animals_data)
+
 
 if __name__ == "__main__":
     main()
